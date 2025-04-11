010534f8a90934f0e26578cefba7513e13569919
kSanrase <klimenko.aro@students.dvfu.ru>
2025-04-11 18:35:54 +1000

    Add Base server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,21 @@
-export {}
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+
+app.use('/static', express.static(path.join(__dirname, '..', 'client')));
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'client', 'index.html'));
+});
+
+const routes = require('./routes/routs');
+app.use('/api', routes);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
